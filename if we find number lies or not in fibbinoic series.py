5e867cf24cf1b58dd9be175aa210daada3d2b0fa
vishalyadav0987 <viahalyadav0987@gmail.com>
2022-12-25 16:43:27 +0530

    nothing

--- if we find number lies or not in fibbinoic series.py
+++ if we find number lies or not in fibbinoic series.py
@@ -1,70 +1,46 @@
-# import random
-# x=[1,2,3,4,5,6]
-# d=random.choice(x)
-# print(d)
+L=[]
+for i in range(int(input("How many digits you type:--"))):
+    i=int(input("--"))
+    L.append(i)
+hi=max(L)
+# print(L)
+x=int(hi)
+a=0
+b=1
+z=0
+l=[]
+if x<0:
+    print("only gives postive values")
+elif x==1:
+    print(a)
+else:
+    while z<x:
+        l.append(a)
+        p=a+b
+        a=b
+        b=p
+        z=z+1
+    # print(l)
+         
+    length=len(L)
+    for j in range(length):
+        if L[j] in l:
+            print("valid",L[j])
+        elif L[j] not in l:
+            print("invalid",L[j]) 
+        else:
+            print("this number is not exsist")    
+               
+           
+                    
 
-# l=[]
-# user=input("take mail from the user:---")
-# for i in user:
-#     l.append(i)
-# print(l)
-
-
-
-#question 1
-
-# i=0
-# l=[]
-# n=int(input("the user-----"))
-# while i<n:
-#     i=i+2
-#     l.append(i) 
-# a=l[0]   
-# b=int(a**2)
-# x=l[1]
-# z=int(x**2)
-# c=l[2]
-# e=int(c**2)
-# print(b+z+e)
-
-
-
-
-
-# ques 2
-
-
-# n=int(input("a---- "))
-# i=1
-# while n>0:
-#     if i%2==1:
-       
-#         print(i,end=" ") 
-#         n=n-1  
-#     i=i+1           
         
 
 
-# n=int(input("a---"))
-# i=1
-# while n>0:
-#     if i%2==0:
-#         m=i**2
-#         print(m,end=" ")
-#         n=n-2
-#     i=i+1       
 
+     
 
-
-
-
-user=input("take mail from the user:---")
-
-i=0
-l=[]
-while i<len(user):
-    l.append(user[i])
-    i=i+1 
-print("Username:--",l[-9:],"domain:--",l[:-10])    
-print()  
-print()     
+    
+     
+    
+          
