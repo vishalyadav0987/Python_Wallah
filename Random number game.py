df5c000eeda09feee51876493f48601d57dc0425
vishalyadav0987 <viahalyadav0987@gmail.com>
2022-12-25 21:08:48 +0530

    nothing

---
+++ Random number game.py
@@ -0,0 +1,30 @@
+
+z=0
+print('''for play say's
+        Y     or    N   ''')
+while True:
+   
+    for_play=input("you want to play:----")
+    if for_play=="y" or for_play=="Y":
+        import random
+        x=int(input("take no from the user(1,6):---"))
+        p=random.randint(1,6)
+        print("Robot choosed by no. own mind:---",p)
+        if p==x:
+            print("Yeah!! you are winner")
+        elif p!=x:
+            print("Btter Luck Next Time:lose the game!")
+    else:
+        print("maa chudao")  
+        break        
+             
+        
+    
+        
+
+
+      
+        
+    
+       
+    
