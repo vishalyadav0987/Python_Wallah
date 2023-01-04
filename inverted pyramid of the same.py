b41933871c07bd1113078c9daaa480bf0154f6cf
vishalyadav0987 <viahalyadav0987@gmail.com>
2023-01-04 21:46:51 +0530

    nothing

---
+++ inverted pyramid of the same.py
@@ -0,0 +1,8 @@
+# # pattern 5.
+n=int(input("Range : "))
+print("Pattern #5: Inverted Pyramid of the Same ")
+n=5
+for i in range(0,n):
+    for j in range(i,n):
+        print(n,end=" ")
+    print()    
