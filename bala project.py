89bd2a533aa5a9b8725b0eabe9a858767ca78721
vishalyadav0987 <viahalyadav0987@gmail.com>
2023-01-08 22:52:54 +0530

    un-comment

--- bala project.py
+++ bala project.py
@@ -1,9 +1,9 @@
-# x=int(input("DO you want's how many rows of matrix 'm' : "))
-# y=int(input("DO you want's how many columns of matrix 'n' : "))
-# for i in range(1,x+1):
-#     for j in range(i,i+y):
-#         print(j,end=" ")
-#     print()
+x=int(input("DO you want's how many rows of matrix 'm' : "))
+y=int(input("DO you want's how many columns of matrix 'n' : "))
+for i in range(1,x+1):
+    for j in range(i,i+y):
+        print(j,end=" ")
+    print()
 
 
 
