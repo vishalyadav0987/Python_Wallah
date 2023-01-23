6e880aff130e00740798106d9604562f2a9d7123
vishalyadav0987 <viahalyadav0987@gmail.com>
2023-01-23 22:08:49 +0530

    nothing

---
+++ Mirrored Pyramid (Right-angled Triangle) Pattern of Numbers.py
@@ -0,0 +1,59 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 
+
+
+
+# pattern 16.
+# print("Pattern #15: Mirrored Pyramid (Right-angled Triangle) Pattern of Numbers")
+n=int(input("Range : "))
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")        
+    for j in range(i,i+i):
+        print("*",end=" ")   
+    print() 
+
+
+
+
+
+
+
+          
+      
+
+
+
+
+
+
+
+
+
+
+
+
+    
