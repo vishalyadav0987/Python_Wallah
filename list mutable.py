505558db5198920530c487c8a15ffe0dec2388c2
vishalyadav0987 <viahalyadav0987@gmail.com>
2023-02-02 23:17:06 +0530

    nothing

---
+++ list mutable.py
@@ -0,0 +1,40 @@
+# l=[1,2,"python",[3,4,"vishal"]]
+# print(l)
+# print(l[0])
+# print(l[-1])
+# print(l[3][2])
+# print(l[3][-1])
+# l[0]="vishal"
+# print(l)
+# l[3][0]="vishal"
+# print(l)
+# l[0:3]=["python","code","help"]
+# print(l)
+# l.append("x")
+# print(l)
+# l.extend([1,2,3,4,5])
+# print(l)
+# l.insert(2,"express")
+# print(l)
+# l1=[1,2,3,4,5]
+# print(l+l1)
+# print(l[0]*3)
+# print(l.index("python"))
+# print(l.pop(2))
+# # print(l.remove(4))
+# # print(l.sort(l1))
+# list=[1,2,3,4,5,6,7,8,9]
+# del (list[1])
+# print(list)
+# list.remove(5)
+# print(list)
+# list.pop()  # removing from the last jab hum value nh denge
+# print(list)
+# print(list.count(7))
+# print(list.index(4))
+
+# list.clear()
+# print(list)
+x=[1,2,3,4,5,6,78]
+print(max(x))
+
