69a7ce4b087e496a3cf6b12a8518f959bc60c8c7
vishalyadav0987 <viahalyadav0987@gmail.com>
2023-02-05 21:12:58 +0530

    nothing

---
+++ dictionary mutable.py
@@ -0,0 +1,58 @@
+# d={"name":"vishal","age":18}
+# print(d["name"])
+# print(d.get("age"))
+# d['name']="vishal yadav"
+# d["class"]=12
+# print(d)
+# d1={1:1,2:4,3:9,4:16,5:25}
+# print(d1.pop(4))
+# print(d1)
+# print(d1.popitem())
+# # d1.clear()
+# d1.copy()
+# print(d1)
+# print(d.items())
+# print(d.keys())
+# print(d.values())
+# d.update(d1)
+# print(d)
+# # d.setdefault({"name":"vishal"})
+# # print(d)
+# print(all(d))
+# print(any(d))
+# print(len(d))
+# print(sorted(d1))
+
+# l={}
+# l.update({1:2})
+# print(l)
+
+# print(l)
+
+
+# l=[1,7,1,2,3,5,6,1,2,3,3,3]
+# b=l.count(6)
+# o={}
+# o.update({int(b):"h"})
+# print(o)
+# d={"vish":5,"dish":6,"bid":7}
+# print(d[6])
+
+# string=input()
+# k=int(input())
+# def partition(string,k):
+#     l=[]
+#     for i in string:
+#         l.append(i)
+#     print(l) 
+#     l2=[]
+#     a=0
+#     for j in range(3,len(l)+3):
+#         if j%3==0:
+#             l2.append(l[a:j])
+#             a+=j
+#     print(l2)    
+
+                     
+        
+# partition(string,k)        
