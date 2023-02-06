5ab6ced2e55067b5376909f5d316bb7ad7cca7bc
vishalyadav0987 <viahalyadav0987@gmail.com>
2023-02-06 23:33:54 +0530

    nothing

---
+++ function within fuction.py
@@ -0,0 +1,125 @@
+# n=int(input("how many you want no of factorial : "))
+# def fact(n):     {functin is 988 tak run ho sakta hai}
+#     if n==1:
+#         return n
+#     else:
+#         return n*fact(n-1)   #fuction call.
+
+# print(fact(n))            
+
+# n=int(input("how many you want no of factorial : "))
+# s=1
+# for i in range(n,0,-1):
+#     s*=i
+# print(s)    
+
+
+# import sys
+# sys.setrecursionlimit(10**6)  # it is used limit in how you want no's of factorial
+
+# # n=int(input("how many you want no of factorial : ")) # fuction sirf 99999 tak hi run hoga
+# def fact(n):
+#     if n==1:
+#         return n
+#     else:
+#         return n*fact(n-1)   #fuction call.
+
+# print(fact(4567))
+
+
+# renaming module
+# import def1 as p
+# p.sum()
+# print()
+
+
+# import math
+# a=math.gcd(24,8)
+# print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
